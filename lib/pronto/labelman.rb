d9cee1b188273eb32c03dcf8f626510b4c77c548
Amiel Perez <perezamiel@yahoo.com>
2017-03-07 11:27:33 +0800

    wip

--- lib/pronto/labelman.rb
+++ lib/pronto/labelman.rb
@@ -2,7 +2,11 @@ module Pronto
   class Labelman < Runner
     def run
       return [] unless @patches
-      p @patches
+      @patches.each do |patch|
+        p "#{patch.new_file_full_path}"
+        p "#{patch.lines}"
+      end
+      return []
     end
   end
 end
--- lib/pronto/labelman/version.rb
+++ lib/pronto/labelman/version.rb
@@ -1,5 +1,5 @@
 module Pronto
   module LabelmanVersion
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
   end
 end
